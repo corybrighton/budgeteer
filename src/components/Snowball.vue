bb36fb74dfaffa59f68a8511565c27e79e75d76d
corybrighton <corysairplane@gmail.com>
2020-12-03 14:20:32 -0700

    Stubbing complet

---
+++ src/components/Snowball.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1> <i class="fas fa-chart-area"></i> Snowball</h1>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class Snowball extends Vue {
+
+  }
+</script>
---
+++ src/components/Worth.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1> <i class="fas fa-chart-line"></i> Worth</h1>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class Worth extends Vue {
+
+  }
+</script>
