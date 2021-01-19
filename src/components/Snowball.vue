1c96223af54ddd14032fe8d1cc34837b75db7981
corybrighton <corysairplane@gmail.com>
2021-01-19 16:39:01 -0700

    canvas built on snowball

--- src/components/Snowball.vue
+++ src/components/Snowball.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1> <i class="fas fa-chart-area"></i> Snowball</h1>
+  <div>
+    <h1> <i class="fas fa-chart-area"></i> Snowball</h1>
+    <canvas id="c"></canvas>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +10,17 @@
 
   @Component
   export default class Snowball extends Vue {
+    vueCanvas;
+    rectWidth = 100;
 
+    mounted() {
+      this.vueCanvas = document.getElementById("c").getContext("2d");
+      this.vueCanvas.clearRect(0, 0, 400, 200);
+
+      // draw rect
+      this.vueCanvas.beginPath();
+      this.vueCanvas.rect(20, 20, this.rectWidth, 100);
+      this.vueCanvas.stroke();
+    }
   }
 </script>
